8278b460070d7d8c48d45d0603dca7e51b77ddc9
dieudonneallognon <jdm231099@gmail.com>
2021-03-11 11:03:09 +0100

    Update traitement_inscription.php

--- traitements/traitement_inscription.php
+++ traitements/traitement_inscription.php
@@ -4,17 +4,17 @@ require_once('../db-config.php');
 
 $_SESSION['err'] = 1;
 
-// On se connecte à MySQL
-// try {
-//     $bdd = new PDO(
-//         $_ENV['DB_SYS'].':host='.$_ENV['DB_HOST'].';dbname='.$_ENV['DB_NAME'],
-//         $_ENV['DB_USERNAME'],
-//         $_ENV['DB_PASSWORD']
-//     );
-// } catch (Exception $e) {
-//     // En cas d'erreur, on affiche un message et on arrête tout
-//     die('Erreur : '.$e->getMessage());
-// }
+//On se connecte à MySQL
+try {
+    $bdd = new PDO(
+        $_ENV['DB_SYS'].':host='.$_ENV['DB_HOST'].';dbname='.$_ENV['DB_NAME'],
+        $_ENV['DB_USERNAME'],
+        $_ENV['DB_PASSWORD']
+    );
+} catch (Exception $e) {
+    // En cas d'erreur, on affiche un message et on arrête tout
+    die('Erreur : '.$e->getMessage());
+}
 
 // $req = $bdd->query('SET NAMES "utf8"');
 
