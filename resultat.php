0b656c4f796750d87d6719fa904721aa708de8a4
dieudonneallognon <jdm231099@gmail.com>
2021-03-11 08:53:44 +0100

    Auto link update

--- resultat.php
+++ resultat.php
@@ -1,246 +1,217 @@
 <?php
-	session_start(); require_once('traitements/db-config.php');
-	$_SESSION['id_mat'] = $_GET['id'];
-	$_SESSION['nm_mat'] = $_SESSION[$_GET['id']];
+    session_start();
+    require_once('traitements/db-config.php');
+    $_SESSION['id_mat'] = $_GET['id'];
+    $_SESSION['nm_mat'] = $_SESSION[$_GET['id']];
 ?>
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Résultats</title>
-		<?php include("includes/head.html"); ?>
-		<link rel="stylesheet" type="text/css" href="css/resultat.css">
-	</head>
+    <head>
+        <title>Résultats</title>
+        <?php include("includes/head.html"); ?>
+        <link rel="stylesheet" type="text/css" href="css/resultat.css">
+    </head>
 
-	<body>
-		<div class="container-fluid">
+    <body>
+        <div class="container-fluid">
 
-			<?php include("includes/navbar-user.html"); ?>
+            <?php include("includes/navbar-user.html"); ?>
 
-			<div class="row">
-				<section class="col-sm-6 col-sm-offset-3">
-					<h1>Résultats</h1><br>
+            <div class="row">
+                <section class="col-sm-6 col-sm-offset-3">
+                    <h1>Résultats</h1><br>
 
-					<?php
-						if (isset($_SESSION['id_mat'])) {
-							if ($_SESSION['id_mat'] == 'edu001') {
-								include("includes/includes_matieres/programmation_web.html");
-							}
-							else if ($_SESSION['id_mat'] == 'edu002') {
-								include("includes/includes_matieres/administration_linux.html");
-							}
-							else if ($_SESSION['id_mat'] == 'edu004') {
-								include("includes/includes_matieres/architecture_ordinateurs.html");
-							}
-							else echo '<h3>Matière indisponible !</h3>';
-						}
-					
-						try
-						{	// On se connecte à MySQL
-							$bdd = new PDO($_ENV['DB_SYS'].':host='.$_ENV['DB_HOST'].';dbname='.$_ENV['DB_NAME'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
-						}
+                    <?php
+                    /*if (isset($_SESSION['id_mat'])) {
+                        if ($_SESSION['id_mat'] == 'edu001') {
+                            include("includes/includes_matieres/programmation_web.html");
+                        } elseif ($_SESSION['id_mat'] == 'edu002') {
+                            include("includes/includes_matieres/administration_linux.html");
+                        } elseif ($_SESSION['id_mat'] == 'edu004') {
+                            include("includes/includes_matieres/architecture_ordinateurs.html");
+                        } else {
+                            echo '<h3>Matière indisponible !</h3>';
+                        }
+                    }*/
+                    include("includes/includes_matieres/programmation_web.html");
 
-						catch(Exception $e)
-						{
-							// En cas d'erreur, on affiche un message et on arrête tout
-							die('Erreur : '.$e->getMessage());
-						}
+                    try {    // On se connecte à MySQL
+                        $bdd = new PDO($_ENV['DB_SYS'].':host='.$_ENV['DB_HOST'].';dbname='.$_ENV['DB_NAME'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+                    } catch (Exception $e) {
+                        // En cas d'erreur, on affiche un message et on arrête tout
+                        die('Erreur : '.$e->getMessage());
+                    }
 
-						$req = $bdd->query('SET NAMES "utf8"');
+                    $req = $bdd->query('SET NAMES "utf8"');
 
-						$rep = $bdd->prepare('SELECT vraie_rep FROM corriger WHERE id_mat=? ORDER BY id_question');
-						$rep->execute(array($_SESSION['id_mat'])) or die(print_r($bdd->errorInfo()));
+                    $rep = $bdd->prepare('SELECT vraie_rep FROM corriger WHERE id_mat=? ORDER BY id_question');
+                    $rep->execute(array($_SESSION['id_mat'])) or die(print_r($bdd->errorInfo()));
 
-						$vRep;
+                    $vRep;
 
-						for ($i=1; $donnees=$rep->fetch(); $i++)
-						{
-							$vRep[$i] = $donnees['vraie_rep'];
-						}
+                    for ($i=1; $donnees=$rep->fetch(); $i++) {
+                        $vRep[$i] = $donnees['vraie_rep'];
+                    }
 
-						$rep->closeCursor();
+                    $rep->closeCursor();
 
-						$rep = $bdd->prepare('SELECT rep_etu FROM repondre WHERE id_mat=? AND matricule=? ORDER BY id_question');
-						$rep->execute(array($_SESSION['id_mat'], $_SESSION['matricule']));
+                    $rep = $bdd->prepare('SELECT rep_etu FROM repondre WHERE id_mat=? AND matricule=? ORDER BY id_question');
+                    $rep->execute(array($_SESSION['id_mat'], $_SESSION['matricule']));
 
-						$eRep;
+                    $eRep;
 
-						for ($i = 1; $donnees = $rep->fetch(); $i++)
-						{
-							$eRep[$i] = $donnees['rep_etu'];
-						}
+                    for ($i = 1; $donnees = $rep->fetch(); $i++) {
+                        $eRep[$i] = $donnees['rep_etu'];
+                    }
 
-						$rep->closeCursor();
+                    $rep->closeCursor();
 
-					?>
-			
-					<div class="row resultat">
-						<div class="col-sm-12 text-center">
-							<legend>Note</legend>
-							<h1 class="note"></h1>
-							<h1 class="mention"></h1>
-						</div>
-					</div>
+                    ?>
+
+                    <div class="row resultat">
+                        <div class="col-sm-12 text-center">
+                            <legend>Note</legend>
+                            <h1 class="note"></h1>
+                            <h1 class="mention"></h1>
+                        </div>
+                    </div>
 
 
-					<div class="row">
-						<div class="col-lg-5 col-lg-offset-1">
-							<a class="btn btn-warning btn-block" <?php echo"href=fichiers_composition/".$_SESSION['matricule']."-".$_SESSION['id_mat'].".txt" ?>>Télécharger mon devoir <span class="glyphicon glyphicon-download"></span></a>
-						</div>
+                    <div class="row">
+                        <!-- <div class="col-lg-5 col-lg-offset-1">
+                            <a class="btn btn-warning btn-block" <?php echo"href=fichiers_composition/".$_SESSION['matricule']."-".$_SESSION['id_mat'].".txt" ?>>Télécharger mon devoir <span class="glyphicon glyphicon-download"></span></a>
+                        </div> -->
 
 
+                        <!-- <div class="col-lg-5 col-lg-offset-1">
+                            <a class="btn btn-primary btn-block" <?php echo"href=énoncés_composition/".$_SESSION['id_mat'].".pdf" ?>>
+                                Télécharger l'enoncé <span class="glyphicon glyphicon-download"></span></a>
+                        </div> -->
 
-						<div class="col-lg-5 col-lg-offset-1">
-							<a class="btn btn-primary btn-block" <?php echo"href=énoncés_composition/".$_SESSION['id_mat'].".pdf" ?>>
-								Télécharger l'enoncé <span class="glyphicon glyphicon-download"></span></a>
-						</div>
-						
-						
-					</div>
+                        <div class="col-lg-6 col-lg-offset-3">
+                            <a class="btn btn-primary btn-block" <?php echo"href=énoncés_composition/edu001.pdf" ?>>
+                                Télécharger l'enoncé <span class="glyphicon glyphicon-download"></span></a>
+                        </div>
 
-					<div class="row">
-						<div class="col-sm-4 col-sm-offset-4">
-							<a class="btn btn-success btn-block" href="matieres.php">OK <span class="glyphicon glyphicon-ok-sign"></span> </a>
-						</div>
-					</div>
+                    </div>
 
-				</section>
-			</div>
+                    <div class="row">
+                        <div class="col-sm-4 col-sm-offset-4">
+                            <a class="btn btn-success btn-block" href="matieres.php">OK <span class="glyphicon glyphicon-ok-sign"></span> </a>
+                        </div>
+                    </div>
 
-			<?php include("includes/footer.html"); ?>
+                </section>
+            </div>
+
+            <?php include("includes/footer.html"); ?>
 
 
-		</div>
-		<script src="js/resultat.js"></script>
+        </div>
+        <script src="js/resultat.js"></script>
 
-		<?php
-			function numReponse($x)
-			{
-				switch ($x)
-				{
-					case 'a':
-						return 1;
-						break;
+        <?php
+        function numReponse($x)
+        {
+            switch ($x) {
+                case 'a':
+                    return 1;
+                    break;
 
-					case 'b':
-						return 2;
-						break;
+                case 'b':
+                    return 2;
+                    break;
 
-					case 'c':
-						return 3;
-						break;
-				}
-			}
+                case 'c':
+                    return 3;
+                    break;
+            }
+        }
 
-			echo "<script>
-					$(function() {
-						";
-		
-				for ($i=1; $i <= 10; $i++)
-				{
-					$posArticle = $i+2;
+        echo "<script>
+                $(function() {
+                    ";
 
-					if (isset($eRep[$i]) && $eRep[$i] == $vRep[$i])
-					{
-						echo "$('article:nth-child(".$posArticle.")').addClass('panel-success');
-						";
-						echo "$('article:nth-child(".$posArticle.") li:nth-child(".numReponse($vRep[$i]).")').addClass('vrai');
-						";
-					}
-					else if (isset($eRep[$i]) && $eRep[$i] != $vRep[$i]) {
-						echo "$('article:nth-child(".$posArticle.")').addClass('panel-danger');
-						";
-						echo "$('article:nth-child(".$posArticle.") li:nth-child(".numReponse($vRep[$i]).")').addClass('vrai');
-						";
-						echo "$('article:nth-child(".$posArticle.") li:nth-child(".numReponse($eRep[$i]).")').addClass('faux');
-						";
-					}
-					else if (!isset($eRep[$i])) {
-						echo "$('article:nth-child(".$posArticle.")').addClass('panel-danger');
-						";
-						echo "$('article:nth-child(".$posArticle.") li:nth-child(".numReponse($vRep[$i]).")').addClass('vrai');
-						";
-					}
-				}
-		
-			echo 	"});
-				</script>";
-		
-			if (isset($_SESSION['id_mat']) && isset($_SESSION['nm_mat']))
-			{
-				echo '<script>
-						$(function (){
-							$("h1").html("'.$_SESSION['nm_mat'].'");
-							$("title").html("QCM '.$_SESSION['nm_mat'].'");
-						});
-					</script>';							
-			}
+        for ($i=1; $i <= 10; $i++) {
+            $posArticle = $i+2;
 
-			$rep = $bdd->prepare('SELECT note FROM enregistrer WHERE id_mat=? AND matricule=?');
-			$rep->execute(array($_SESSION['id_mat'], $_SESSION['matricule']));
+            if (isset($eRep[$i]) && $eRep[$i] == $vRep[$i]) {
+                echo "$('article:nth-child(".$posArticle.")').addClass('panel-success');";
+                echo "$('article:nth-child(".$posArticle.") li:nth-child(".numReponse($vRep[$i]).")').addClass('vrai');";
+            } elseif (isset($eRep[$i]) && $eRep[$i] != $vRep[$i]) {
+                echo "$('article:nth-child(".$posArticle.")').addClass('panel-danger');";
+                echo "$('article:nth-child(".$posArticle.") li:nth-child(".numReponse($vRep[$i]).")').addClass('vrai');";
+                echo "$('article:nth-child(".$posArticle.") li:nth-child(".numReponse($eRep[$i]).")').addClass('faux');";
+            } elseif (!isset($eRep[$i])) {
+                echo "$('article:nth-child(".$posArticle.")').addClass('panel-danger');";
+                echo "$('article:nth-child(".$posArticle.") li:nth-child(".numReponse($vRep[$i]).")').addClass('vrai');";
+            }
+        }
 
-			if ($donnees = $rep->fetch())
-			{
-				echo '<script>
-						$(function (){
-							$("div.resultat h1.note").html("'.$donnees['note'].'/20");
-							';
-				if ($donnees['note'] >= 18 && $donnees['note'] <= 20)
-				{
-					echo '$("div.resultat h1.mention").html("Mention: Excellente");
-						$("div.resultat h1.mention").css("color", "#08B000");
-						});
-					</script>';
-				}
-				else if($donnees['note'] >= 16 && $donnees['note'] <= 17)
-				{
-					echo '$("div.resultat h1.mention").html("Mention: Très Bien");
-						$("div.resultat h1.mention").css("color", "#8B8B00");
-						});
-					</script>';
-				}
-				else if($donnees['note'] >= 14 && $donnees['note'] <= 15)
-				{
-					echo '$("div.resultat h1.mention").html("Mention: Bien");
-						$("div.resultat h1.mention").css("color", "#EEDD82");
-						});
-					</script>';
-				}
-				else if ($donnees['note'] >= 12 && $donnees['note'] <= 13)
-				{
-					echo '$("div.resultat h1.mention").html("Mention: Assez Bien");
-						$("div.resultat h1.mention").css("color", "#CD8500");
-						});
-					</script>';
-				}
-				elseif ($donnees['note'] >= 10 && $donnees['note'] <= 11)
-				{
-					echo '$("div.resultat h1.mention").html("Mention Passable");
-						$("div.resultat h1.mention").css("color", "#CD7054");
-						});
-					</script>';
-				}
-				elseif ($donnees['note'] >= 8 && $donnees['note'] <= 9)
-				{
-					echo '$("div.resultat h1.mention").html("Mention: Insufisant");
-						$("div.resultat h1.mention").css("color", "#CD4F39");
-						});
-					</script>';
-				}
-				elseif ($donnees['note'] >= 0 && $donnees['note'] <= 7)
-				{
-					echo '$("div.resultat h1.mention").html("Mention: Très Insufisant");
-						$("div.resultat h1.mention").css("color", "#CD0000");
-						});
-					</script>';
-				}	
-			}
+        echo     "});
+            </script>";
 
-			$rep->closeCursor();
-		?>
-	</body>
+        if (isset($_SESSION['id_mat']) && isset($_SESSION['nm_mat'])) {
+            echo '<script>
+                    $(function (){
+                        $("h1").html("'.$_SESSION['nm_mat'].'");
+                        $("title").html("QCM '.$_SESSION['nm_mat'].'");
+                    });
+                </script>';
+        }
+
+        $rep = $bdd->prepare('SELECT note FROM enregistrer WHERE id_mat=? AND matricule=?');
+        $rep->execute(array($_SESSION['id_mat'], $_SESSION['matricule']));
+
+        if ($donnees = $rep->fetch()) {
+            echo '<script>
+                    $(function (){
+                        $("div.resultat h1.note").html("'.$donnees['note'].'/20");
+                        ';
+            if ($donnees['note'] >= 18 && $donnees['note'] <= 20) {
+                echo '$("div.resultat h1.mention").html("Mention: Excellente");
+                    $("div.resultat h1.mention").css("color", "#08B000");
+                    });
+                </script>';
+            } elseif ($donnees['note'] >= 16 && $donnees['note'] <= 17) {
+                echo '$("div.resultat h1.mention").html("Mention: Très Bien");
+                    $("div.resultat h1.mention").css("color", "#8B8B00");
+                    });
+                </script>';
+            } elseif ($donnees['note'] >= 14 && $donnees['note'] <= 15) {
+                echo '$("div.resultat h1.mention").html("Mention: Bien");
+                    $("div.resultat h1.mention").css("color", "#EEDD82");
+                    });
+                </script>';
+            } elseif ($donnees['note'] >= 12 && $donnees['note'] <= 13) {
+                echo '$("div.resultat h1.mention").html("Mention: Assez Bien");
+                    $("div.resultat h1.mention").css("color", "#CD8500");
+                    });
+                </script>';
+            } elseif ($donnees['note'] >= 10 && $donnees['note'] <= 11) {
+                echo '$("div.resultat h1.mention").html("Mention Passable");
+                    $("div.resultat h1.mention").css("color", "#CD7054");
+                    });
+                </script>';
+            } elseif ($donnees['note'] >= 8 && $donnees['note'] <= 9) {
+                echo '$("div.resultat h1.mention").html("Mention: Insufisant");
+                    $("div.resultat h1.mention").css("color", "#CD4F39");
+                    });
+                </script>';
+            } elseif ($donnees['note'] >= 0 && $donnees['note'] <= 7) {
+                echo '$("div.resultat h1.mention").html("Mention: Très Insufisant");
+                    $("div.resultat h1.mention").css("color", "#CD0000");
+                    });
+                </script>';
+            }
+        }
+
+        $rep->closeCursor();
+        ?>
+    </body>
 </html>
 
 <?php
-	unset($_SESSION['nm_mat']);
+unset($_SESSION['nm_mat']);
 ?>
--- traitements/traitement_inscription.php
+++ traitements/traitement_inscription.php
@@ -47,6 +47,23 @@ if ($unique) {
     'MAtière de Test',
     date('H:i:s', strtotime('+ 1 hour 600 seconds'))));
 
+    $req = $bdd->prepare(
+        'INSERT INTO corriger (vraie_rep, id_mat, id_question) VALUES
+        ("b", edu'.$_POST['matricule'].', 1),
+        ("c", edu'.$_POST['matricule'].', 2),
+        ("c", edu'.$_POST['matricule'].', 3),
+        ("c", edu'.$_POST['matricule'].', 4),
+        ("b", edu'.$_POST['matricule'].', 5),
+        ("b", edu'.$_POST['matricule'].', 6),
+        ("b", edu'.$_POST['matricule'].', 7),
+        ("a", edu'.$_POST['matricule'].', 8),
+        ("9", edu'.$_POST['matricule'].', 9),
+        ("10", edu'.$_POST['matricule'].', 10)'
+    );
+
+    $req->execute();
+
+
     header('Location: ../valide.html');
 } else {
     $_SESSION['err_ins'] = 1;
